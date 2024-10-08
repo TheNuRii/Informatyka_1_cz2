4cb73eb26b72c0d582cbc851787d9b829860ccf9
TheNuRii <maciekglow0@gmail.com>
2024-10-08 23:12:07 +0200

    Klasy, konstruktor, destruktor

---
+++ wyklady/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+// Przyklad Klasy
+class Adress {
+    private:
+        string Street, City, Country;
+        int House_number, Zip_code[6];
+    public:
+        void enter_data(string, string, string, int, int[])
+        void display();
+        int get_number(){
+            return House_number;
+        }
+};
+
+//Przyklad Destruktora
+class Cylinder {
+    double raidus;
+public:
+    Cylinderlinder(double r = 1.0) : raidus(r) { }
+    double area() {return 3.14 * raidus *  raidus;}
+    // destruktor
+    ~Circle(){
+        std::cout<<"Cilinder descructor in use.";
+    }
+};
+
+// Liczby zespolone
+class complex {
+public:
+    double re, im;
+    complex (double r, double  i) : re(r), im(i){}
+};
+
+complex Csum (complex a1, complex a2){
+    complex sum(0, 0);
+    sum.re = a1.re + a2.re;
+    sum.im = a1.im + a2.im;
+    return sum;
+}
+
+complex Cmult (complex a1, complex a2){
+    complex product(0 , 0);
+    product.re = a1.re * a2.re - a1.im * a2.im;
+    product.im = a1.re * a2.im + a1.im * a2.re;
+    return product;
+}
+
+int main(){
+    Adress my_address, university, my_girl_friend;
+    int where = my_gril_friend.get_number();
+
+    Cilinder column(10, 20);
+    column.~Cylinder();
+
+}
